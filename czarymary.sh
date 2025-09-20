0c71aeaa8af3c6a2c45b25e46598a96096115203
Sebastian Fyda <UncleTawnos@users.noreply.github.com>
2025-09-20 22:00:42 +0200

    Fix directory name from bashrc.bashrc.d to bash.bashrc.d

--- czarymary.sh
+++ czarymary.sh
@@ -6,7 +6,7 @@ apt install -y patch gawk bash-completion vim-nox net-tools aptitude git
 mkdir -p /etc/apt/conf.d
 wget https://raw.githubusercontent.com/UncleTawnos/bashmash/refs/heads/master/etc/apt/conf.d/recommends.conf -O /etc/apt/conf.d/recommends.conf
 wget https://raw.githubusercontent.com/UncleTawnos/bashmash/refs/heads/master/vimrc.local -O /etc/vim/vimrc.local
-mkdir -p /etc/bashrc.bashrc.d
+mkdir -p /etc/bash.bashrc.d
 wget https://raw.githubusercontent.com/UncleTawnos/bashmash/refs/heads/master/system_aliases -O /etc/bash.bashrc.d/system_aliases
 
 cd /tmp
